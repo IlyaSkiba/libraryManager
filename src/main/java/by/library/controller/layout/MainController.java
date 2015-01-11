d0e054df4d4031188e55bdc5efe17e8a9bf4350d
Ilya <ilya.skiba@gmail.com>
2015-01-11 22:27:44 +0300

    Project init

---
+++ src/main/java/by/library/controller/layout/MainController.java
@@ -0,0 +1,10 @@
+package by.library.controller.layout;
+
+import javafx.scene.control.Button;
+
+/**
+ * Created by Ilya on 11.01.2015.
+ */
+public class MainController {
+    public Button reindexInvoke;
+}
